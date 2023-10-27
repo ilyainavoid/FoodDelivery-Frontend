96c26fcfd02e74143630c23caebb0091152730ef
Ilya <thirdilno@gmail.com>
2023-10-27 21:13:41 +0700

    feat(token-storage): add token storing in cookie
    setCookie(name, value, daysToExpire) sets a cookie with token in it
    getToken(name) gets a token under a given name

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,13 +1,6 @@
-function getCookie(name) {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-const authToken = getCookie("authToken");
+const authToken = "aaa";
 const authorizedElements = document.querySelectorAll(".authorized");
 const unauthorizedElements = document.querySelectorAll(".unauthorized");
-console.log(authToken);
 
 if (authToken) {
 	
@@ -15,6 +8,7 @@ if (authToken) {
 			element.style.display = "block";
 	});
 
+	
 	unauthorizedElements.forEach(function(element) {
 			element.style.display = "none";
 	});
---
+++ scripts/token-handling.js
@@ -0,0 +1,23 @@
+function setCookie(name, token, daysToExpire) {
+  const date = new Date();
+  date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${token}; ${expires}; path=/; secure; samesite=strict;`;
+}
+
+function getToken(name) {
+  const cookieName = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
