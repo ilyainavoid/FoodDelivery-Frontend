a0fe56e74bd6fead7183bf2308f0c21363b462d2
Ilya <thirdilno@gmail.com>
2023-10-27 21:21:09 +0700

    feat(homepage): add profile and logout button
    add profile and logout button in header for authorized user

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,13 +1,7 @@
-function getCookie(name) {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
-}
 
-const authToken = getCookie("authToken");
 const authorizedElements = document.querySelectorAll(".authorized");
 const unauthorizedElements = document.querySelectorAll(".unauthorized");
-console.log(authToken);
+
 
 if (authToken) {
 	
