61a2d0ebebd772020828f9ad3007e4f17cb337df
Ilya <thirdilno@gmail.com>
2023-11-04 02:21:19 +0700

    fix(homepage): fix wrong header display
    fix issue when header for authorized user was displayed to unauthorized
    the issue was in if statement, the ! sign was setting the opposite value
    that's why this code worked in a wrong way

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	let authorized = localStorage.getItem('authorized');
-	if(!authorized) {
+	if(authorized) {
 		document.querySelector('.header-top-authorized').classList.remove("d-none");
 	} else {
 		document.querySelector('.header-top-unauthorized').classList.remove("d-none");
